259dd3ca39a8ce5b26ceab328cad46d8e583c353
Nat Tanchotchuang <sakanomakeit@gmail.com>
2022-01-19 21:14:36 +0700

    Create Nabvar
    -

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  pageName: string = '';
+  username: string = 'ไม่ระบุตัวตน';
+  visibleToggleMenu: boolean = false;
 
-  constructor() { }
+  constructor(
+    private readonly router: Router,
+    private readonly route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onLogout(): void {
+    this.router.navigate(['login'])
   }
-
 }
--- src/app/core/components/navbar/navbar.module.ts
+++ src/app/core/components/navbar/navbar.module.ts
@@ -1,9 +1,10 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [NavbarComponent],
   exports: [NavbarComponent],
 })
